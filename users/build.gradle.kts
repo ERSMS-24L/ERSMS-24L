6ec84d1d09b712b2159e3cfcf00f0ef71d952edd
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2024-03-09 22:37:45 +0100

    [setup][workflow] Fix workflow build

--- users/build.gradle.kts
+++ users/build.gradle.kts
@@ -35,8 +35,8 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
-	testImplementation("org.axonframework.extensions.reactor:axon-reactor-spring-boot-starter")
-	testImplementation("org.axonframework:axon-test")
+//	testImplementation("org.axonframework.extensions.reactor:axon-reactor-spring-boot-starter")
+//	testImplementation("org.axonframework:axon-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.testcontainers:junit-jupiter")
