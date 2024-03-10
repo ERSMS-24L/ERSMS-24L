89d2701f3e05e04bf0d3af5a27d27e2dc3a09563
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2024-03-10 23:44:44 +0100

    [setup][users]Update validator constraints

--- users/src/main/kotlin/pl/edu/pw/ia/users/application/UserController.kt
+++ users/src/main/kotlin/pl/edu/pw/ia/users/application/UserController.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import java.util.UUID
 import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway
 import org.springframework.core.convert.ConversionService
@@ -53,7 +54,7 @@ class UserControllerImpl(
 	@ResponseStatus(HttpStatus.CREATED)
 	@PreAuthorize("hasAnyAuthority('${Scopes.USER.WRITE}')")
 	override fun createUser(
-		@RequestBody request: CreateUserRequest
+		@Valid @RequestBody request: CreateUserRequest
 	): Mono<UUID> {
 		val command = conversionService.convert(request, CreateUserCommand::class.java)
 		return reactorCommandGateway.send(command)
--- users/src/main/kotlin/pl/edu/pw/ia/users/application/model/CreateUserRequest.kt
+++ users/src/main/kotlin/pl/edu/pw/ia/users/application/model/CreateUserRequest.kt
@@ -2,19 +2,19 @@ package pl.edu.pw.ia.users.application.model
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Email
-import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
 
 data class CreateUserRequest(
 
 	@Schema(maxLength = 50)
 	@field:NotBlank(message = "Name cannot be blank")
-	@field:Max(value = 50, message = "Maximum allowed name length is 50 characters")
+	@field:Length(max = 50, message = "Maximum allowed name length is 50 characters")
 	val name: String,
 
 	@Schema(maxLength = 320)
 	@field:Email
 	@field:NotBlank(message = "Email cannot be blank")
-	@field:Max(value = 320, message = "Maximum allowed email length is 320 characters")
+	@field:Length(max = 50, message = "Maximum allowed email length is 320 characters")
 	val email: String,
 )
