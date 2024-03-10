aae07173629ab5c5791fda5ae97cd069affafe2c
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2024-03-10 23:57:39 +0100

    [setup][users] Configure mongo testcontainer

--- users/src/test/kotlin/pl/edu/pw/ia/users/UsersApplicationTests.kt
+++ users/src/test/kotlin/pl/edu/pw/ia/users/UsersApplicationTests.kt
@@ -3,7 +3,7 @@ package pl.edu.pw.ia.users
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [TestUsersApplication::class])
 class UsersApplicationTests {
 
 	@Test
