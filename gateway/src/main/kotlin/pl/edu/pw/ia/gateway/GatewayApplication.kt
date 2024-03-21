8b8f0e6a474fd392a39004ed2a1c40e9aebcd1d7
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2024-03-21 11:54:29 +0100

    [feat][k8s] Configure base paths for services

--- gateway/src/main/kotlin/pl/edu/pw/ia/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/pl/edu/pw/ia/gateway/GatewayApplication.kt
@@ -10,8 +10,6 @@ import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder
 import org.springframework.cloud.client.circuitbreaker.Customizer
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.gateway.route.RouteLocator
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.web.reactive.config.EnableWebFlux
 
@@ -20,32 +18,6 @@ import org.springframework.web.reactive.config.EnableWebFlux
 @SpringBootApplication
 @EnableDiscoveryClient
 class GatewayApplication {
-
-	@Bean
-	fun routeLocator(builder: RouteLocatorBuilder): RouteLocator {
-		return builder.routes()
-			.route("accounts") { predicate ->
-				predicate.path("/accounts/**")
-					.filters { it.stripPrefix(1) }
-					.uri("lb://ACCOUNTS")
-			}
-			.route("posts") { predicate ->
-				predicate.path("/posts/**")
-					.filters { it.stripPrefix(1) }
-					.uri("lb://POSTS")
-			}
-			.route("threads") { predicate ->
-				predicate.path("/threads/**")
-					.filters { it.stripPrefix(1) }
-					.uri("lb://THREADS")
-			}
-			.route("discovery") { predicate ->
-				predicate.path("/discovery/**")
-					.filters { it.stripPrefix(1) }
-					.uri("lb://DISCOVERY")
-			}
-			.build()
-	}
 
 	@Bean
 	fun defaultCustomizer(): Customizer<ReactiveResilience4JCircuitBreakerFactory> {
