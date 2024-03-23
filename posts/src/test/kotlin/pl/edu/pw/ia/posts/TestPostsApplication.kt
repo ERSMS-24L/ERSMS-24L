9f58834a6f09eae54be0f6f4b8689c4e6e76257b
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2024-03-24 00:15:20 +0100

    [fix][posts&threads] Fix integration tests

---
+++ posts/src/test/kotlin/pl/edu/pw/ia/posts/TestPostsApplication.kt
@@ -0,0 +1,25 @@
+package pl.edu.pw.ia.posts
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.boot.with
+import org.springframework.context.annotation.Bean
+import org.testcontainers.containers.MongoDBContainer
+import org.testcontainers.utility.DockerImageName
+import pl.edu.pw.ia.shared.SharedApplication
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestPostsApplication {
+
+	@Bean
+	@ServiceConnection
+	fun mongoDbContainer(): MongoDBContainer {
+		return MongoDBContainer(DockerImageName.parse("mongo:latest"))
+	}
+
+}
+
+fun main(args: Array<String>) {
+	fromApplication<SharedApplication>().with(TestPostsApplication::class).run(*args)
+}
--- threads/src/test/kotlin/pl/edu/pw/ia/threads/ThreadsApplicationTests.kt
+++ threads/src/test/kotlin/pl/edu/pw/ia/threads/ThreadsApplicationTests.kt
@@ -3,11 +3,10 @@ package pl.edu.pw.ia.threads
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [TestThreadsApplication::class])
 class ThreadsApplicationTests {
 
 	@Test
 	fun contextLoads() {
 	}
-
 }
