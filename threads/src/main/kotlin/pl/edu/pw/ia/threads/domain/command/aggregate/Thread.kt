a089cd8618205e74c903f2302fc0ccdf3d341811
Konrad Wojda <konrad2001@outlook.com>
2024-04-01 21:22:11 +0200

    Add DeleteThreadCommand handler

--- threads/src/main/kotlin/pl/edu/pw/ia/threads/domain/command/aggregate/Thread.kt
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/domain/command/aggregate/Thread.kt
@@ -6,8 +6,10 @@ import org.axonframework.modelling.command.AggregateIdentifier
 import org.axonframework.modelling.command.AggregateLifecycle
 import org.axonframework.spring.stereotype.Aggregate
 import pl.edu.pw.ia.shared.domain.command.CreateThreadCommand
+import pl.edu.pw.ia.shared.domain.command.DeleteThreadCommand
 import pl.edu.pw.ia.shared.domain.command.UpdateThreadCommand
 import pl.edu.pw.ia.shared.domain.event.ThreadCreatedEvent
+import pl.edu.pw.ia.shared.domain.event.ThreadDeleteEvent
 import pl.edu.pw.ia.shared.domain.event.ThreadUpdatedEvent
 import pl.edu.pw.ia.shared.domain.exception.AccountMismatchException
 import java.time.Instant
@@ -48,6 +50,23 @@ internal class Thread {
                 accountId = command.accountId,
                 title = command.title,
                 threadId = command.threadId,
+            )
+        )
+    }
+
+    @CommandHandler
+    fun handle(command: DeleteThreadCommand) {
+        if (accountId != command.accountId) {
+            throw AccountMismatchException(
+                currentAccountId = command.accountId,
+                ownerAccountId = accountId,
+            )
+        }
+        // TODO: Check if user is administrator
+        AggregateLifecycle.apply(
+            ThreadDeleteEvent(
+                accountId = command.accountId,
+                threadId = command.threadId
             )
         )
     }
