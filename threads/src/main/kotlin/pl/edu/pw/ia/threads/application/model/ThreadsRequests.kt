17853013c554aa813fc349108fac4c40be0a7778
Konrad Wojda <konrad2001@outlook.com>
2024-04-01 21:21:34 +0200

    Add ThreadDeleteRequest

--- threads/src/main/kotlin/pl/edu/pw/ia/threads/application/model/ThreadsRequests.kt
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/application/model/ThreadsRequests.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import org.hibernate.validator.constraints.Length
 import pl.edu.pw.ia.shared.domain.command.CreateThreadCommand
+import pl.edu.pw.ia.shared.domain.command.DeleteThreadCommand
 import pl.edu.pw.ia.shared.domain.command.UpdateThreadCommand
 import pl.edu.pw.ia.shared.security.SecurityContext
 import java.util.UUID
@@ -35,6 +36,17 @@ data class ThreadUpdateRequest(
         UpdateThreadCommand(
             threadId = UUID.fromString(threadId),
             title = title,
+            accountId = SecurityContext.getAccountId(),
+        )
+}
+
+data class ThreadDeleteRequest(
+    @field:org.hibernate.validator.constraints.UUID(message = "Thread id is invalid")
+    val threadId: String,
+) {
+    fun toCommand() =
+        DeleteThreadCommand(
+            threadId = UUID.fromString(threadId),
             accountId = SecurityContext.getAccountId(),
         )
 }
