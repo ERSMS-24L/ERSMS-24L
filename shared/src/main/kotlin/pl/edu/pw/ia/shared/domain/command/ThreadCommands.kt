c0ec9998e34fdc3c93fbc5b8e044486d243e4581
Konrad Wojda <konrad2001@outlook.com>
2024-04-01 20:29:40 +0200

    Added TargetAggregateIdentifiers to threads related commands

--- shared/src/main/kotlin/pl/edu/pw/ia/shared/domain/command/ThreadCommands.kt
+++ shared/src/main/kotlin/pl/edu/pw/ia/shared/domain/command/ThreadCommands.kt
@@ -1,15 +1,16 @@
 package pl.edu.pw.ia.shared.domain.command
 
+import org.axonframework.modelling.command.TargetAggregateIdentifier
 import java.util.UUID
 
 data class CreateThreadCommand(
-	val threadId: UUID,
+	@TargetAggregateIdentifier val threadId: UUID,
 	val title: String,
 	val accountId: UUID,
 )
 
 data class UpdateThreadCommand(
-	val threadId: UUID,
+	@TargetAggregateIdentifier val threadId: UUID,
 	val title: String,
 	val accountId: UUID,
 )
