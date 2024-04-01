c94c674c9e0b3a2ce2338080c26a17152eb50f9b
Konrad Wojda <konrad2001@outlook.com>
2024-04-01 20:58:08 +0200

    Added ThreadScopes

--- shared/src/main/kotlin/pl/edu/pw/ia/shared/security/Scopes.kt
+++ shared/src/main/kotlin/pl/edu/pw/ia/shared/security/Scopes.kt
@@ -3,6 +3,7 @@ package pl.edu.pw.ia.shared.security
 object Scopes {
 
 	val USER = UserScopes
+	val THREAD = ThreadScopes
 
 	private const val SCOPE = "SCOPE"
 
@@ -11,5 +12,12 @@ object Scopes {
 
 		const val READ = "${SCOPE}_read:${USERS}"
 		const val WRITE = "${SCOPE}_write:${USERS}"
+	}
+
+	object ThreadScopes {
+		private const val THREADS = "threads"
+
+		const val READ = "${SCOPE}_read:${THREADS}"
+		const val WRITE = "${SCOPE}_write:${THREADS}"
 	}
 }
