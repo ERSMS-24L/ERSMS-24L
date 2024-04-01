606a8e0ff6a92e129796ec4898414da02e1037d9
Konrad Wojda <konrad2001@outlook.com>
2024-04-01 20:29:56 +0200

    Changed version of shared lib

--- threads/build.gradle.kts
+++ threads/build.gradle.kts
@@ -17,7 +17,7 @@ val axonVersion = "4.9.4"
 val springDocVersion = "2.4.0"
 
 dependencies {
-	implementation("pl.edu.pw.ia:ersms-shared:0.2.2")
+	implementation("pl.edu.pw.ia:ersms-shared:threads-dev")
 
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
