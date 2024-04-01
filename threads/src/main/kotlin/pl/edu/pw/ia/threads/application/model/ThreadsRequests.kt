c107c427e20f771ed05552f8e5159e9b1a4218d0
Konrad Wojda <konrad2001@outlook.com>
2024-04-01 21:10:36 +0200

    Added ThreadUpdateRequest

--- threads/src/main/kotlin/pl/edu/pw/ia/threads/application/model/ThreadsRequests.kt
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/application/model/ThreadsRequests.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import org.hibernate.validator.constraints.Length
 import pl.edu.pw.ia.shared.domain.command.CreateThreadCommand
+import pl.edu.pw.ia.shared.domain.command.UpdateThreadCommand
 import pl.edu.pw.ia.shared.security.SecurityContext
 import java.util.UUID
 
@@ -16,6 +17,23 @@ data class ThreadCreateRequest(
     fun toCommand() =
         CreateThreadCommand(
             threadId = UUID.randomUUID(),
+            title = title,
+            accountId = SecurityContext.getAccountId(),
+        )
+}
+
+data class ThreadUpdateRequest(
+    @Schema(maxLength = 256)
+    @field:NotBlank(message = "Title cannot be blank")
+    @field:Length(max = 256, message = "Maximum allowed title length is 256 characters.")
+    val title: String,
+
+    @field:org.hibernate.validator.constraints.UUID(message = "Thread id is invalid")
+    val threadId: String,
+) {
+    fun toCommand() =
+        UpdateThreadCommand(
+            threadId = UUID.fromString(threadId),
             title = title,
             accountId = SecurityContext.getAccountId(),
         )
