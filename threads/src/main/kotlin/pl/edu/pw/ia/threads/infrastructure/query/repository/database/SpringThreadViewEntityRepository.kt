603e680672874225875c72c311f67aea84c77478
Konrad Wojda <konrad2001@outlook.com>
2024-04-02 21:11:04 +0200

    Create counts

--- threads/src/main/kotlin/pl/edu/pw/ia/threads/infrastructure/query/repository/database/SpringThreadViewEntityRepository.kt
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/infrastructure/query/repository/database/SpringThreadViewEntityRepository.kt
@@ -5,11 +5,15 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import pl.edu.pw.ia.threads.infrastructure.query.entity.ThreadViewEntity
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import java.time.Instant
 
 @Repository
 interface SpringThreadViewEntityRepository : ReactiveMongoRepository<ThreadViewEntity, String> {
     fun findByAccountId(accountId: String, pageable: Pageable) : Flux<ThreadViewEntity>
+    fun countByAccountId(accountId: String) : Mono<Long>
     fun findByTitleIsContainingIgnoreCase(title: String, pageable: Pageable) : Flux<ThreadViewEntity>
+    fun countByTitleIsContainingIgnoreCase(title: String) : Mono<Long>
     fun findByLastModifiedGreaterThanEqual(lastModified: Instant, pageable: Pageable) : Flux<ThreadViewEntity>
+    fun countByLastModifiedGreaterThanEqual(lastModified: Instant) : Mono<Long>
 }
