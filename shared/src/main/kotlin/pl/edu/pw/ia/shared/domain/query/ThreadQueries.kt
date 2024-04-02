98ca9272d26eccc2372bb916ed611d34fefedce5
Konrad Wojda <konrad2001@outlook.com>
2024-04-02 20:58:19 +0200

    Define new thread queries

--- shared/src/main/kotlin/pl/edu/pw/ia/shared/domain/query/ThreadQueries.kt
+++ shared/src/main/kotlin/pl/edu/pw/ia/shared/domain/query/ThreadQueries.kt
@@ -2,9 +2,25 @@ package pl.edu.pw.ia.shared.domain.query
 
 import java.util.UUID
 import org.springframework.data.domain.Pageable
+import java.time.Instant
 
 data class FindThreadByIdQuery(
 	val threadId: UUID,
+)
+
+data class FindThreadsByTitleQuery(
+	val title: String,
+	val pageable: Pageable
+)
+
+data class FindRecentThreadsQuery(
+	val date: Instant,
+	val pageable: Pageable
+)
+
+data class FindThreadsByAuthor(
+	val accountId: UUID,
+	val pageable: Pageable
 )
 
 data class FindModeratorByThreadAndAccountIdQuery(
