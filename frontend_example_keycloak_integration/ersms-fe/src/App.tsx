a622470857f462eb4735b83cc42be638cb647101
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2024-05-02 17:07:40 +0200

    FE - BE call via proxy

--- frontend_example_keycloak_integration/ersms-fe/src/App.tsx
+++ frontend_example_keycloak_integration/ersms-fe/src/App.tsx
@@ -8,6 +8,28 @@ function App() {
     return <></>;
   }
 
+  function sendRequest() {
+    const raw = JSON.stringify({
+      name: 'name',
+      email: 'email@email.com',
+    });
+
+    console.log(keycloak.token);
+
+    fetch('/v1/accounts', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + keycloak.token,
+      },
+      body: raw,
+    })
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  }
+
   if (initialized && keycloak.tokenParsed) {
     console.log(keycloak);
     console.log(keycloak.tokenParsed);
@@ -28,6 +50,10 @@ function App() {
           >
             Keycloak logout
           </a>
+          <br />
+          <button onClick={sendRequest}>
+            Send request to protected service endpoint
+          </button>
         </div>
       </div>
     );
@@ -51,6 +77,10 @@ function App() {
         >
           Keycloak login
         </a>
+        <br />
+        <button onClick={sendRequest}>
+          Send request to protected service endpoint
+        </button>
       </div>
     </div>
   );
