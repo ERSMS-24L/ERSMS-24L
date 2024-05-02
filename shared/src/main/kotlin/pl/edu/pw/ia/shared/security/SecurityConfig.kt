cff44b93e8ff871f3defb53697c7bf8e704ebe90
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2024-05-02 17:10:06 +0200

    unused import

--- shared/src/main/kotlin/pl/edu/pw/ia/shared/security/SecurityConfig.kt
+++ shared/src/main/kotlin/pl/edu/pw/ia/shared/security/SecurityConfig.kt
@@ -1,6 +1,5 @@
 package pl.edu.pw.ia.shared.security
 
-import java.security.interfaces.RSAPublicKey
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
