c3de81597c783832121afb481b5134904648ebe7
Konrad Wojda <konrad2001@outlook.com>
2024-05-04 10:27:26 +0200

    Update shared

--- posts/build.gradle.kts
+++ posts/build.gradle.kts
@@ -19,7 +19,7 @@ val axonVersion = "4.9.4"
 val springDocVersion = "2.5.0"
 
 dependencies {
-	implementation("pl.edu.pw.ia:ersms-shared:post-query-dev")
+	implementation("pl.edu.pw.ia:ersms-shared:6ef7b70")
 
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
