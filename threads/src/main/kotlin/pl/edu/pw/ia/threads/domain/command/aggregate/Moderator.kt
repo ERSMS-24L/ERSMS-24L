43a39330a232207cb460ab60344421bc450ebf2f
Konrad Wojda <konrad2001@outlook.com>
2024-05-23 16:08:15 +0200

    Add Moderator aggregate

---
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/domain/command/aggregate/Moderator.kt
@@ -0,0 +1,48 @@
+package pl.edu.pw.ia.threads.domain.command.aggregate
+
+import java.util.UUID
+import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.modelling.command.AggregateIdentifier
+import org.axonframework.modelling.command.AggregateLifecycle
+import org.axonframework.spring.stereotype.Aggregate
+import pl.edu.pw.ia.shared.domain.command.AddModeratorCommand
+import pl.edu.pw.ia.shared.domain.command.RemoveModeratorCommand
+import pl.edu.pw.ia.shared.domain.event.ModeratorAddedEvent
+import pl.edu.pw.ia.shared.domain.event.ModeratorRemovedEvent
+
+@Aggregate
+internal class Moderator {
+
+	@AggregateIdentifier
+	private lateinit var moderatorId: UUID
+	private lateinit var accountId: UUID
+	private lateinit var threadId: UUID
+
+	private constructor()
+
+	@CommandHandler
+	constructor(command: AddModeratorCommand) {
+		// TODO: Check if command.accountId is moderator in command.threadId
+		AggregateLifecycle.apply(
+			ModeratorAddedEvent(
+				moderatorId = command.moderatorId,
+				threadId = command.threadId,
+				accountId = command.subjectAccountId
+			)
+		)
+	}
+
+	@CommandHandler
+	fun handle(command: RemoveModeratorCommand) {
+		// TODO: Check if command.accountId is moderator in command.threadId
+		// TODO: Should we allow moderator to delete himself from moderators?
+		AggregateLifecycle.apply(
+			ModeratorRemovedEvent(
+				moderatorId = command.moderatorId,
+				threadId = command.threadId,
+				accountId = command.subjectAccountId,
+			)
+		)
+	}
+
+}
