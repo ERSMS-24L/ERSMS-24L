01c9f7a23ce90f2f099ebb1895045997a73acaa1
Konrad Wojda <konrad2001@outlook.com>
2024-05-25 15:45:33 +0200

    remove unused import

---
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/infrastructure/query/repository/database/SpringBannedUserViewEntityRepository.kt
@@ -0,0 +1,15 @@
+package pl.edu.pw.ia.threads.infrastructure.query.repository.database
+
+import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.stereotype.Repository
+import pl.edu.pw.ia.threads.infrastructure.query.entity.BannedUserViewEntity
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@Repository
+interface SpringBannedUserViewEntityRepository : ReactiveMongoRepository<BannedUserViewEntity, String> {
+	fun findByAccountIdAndThreadId(accountId: String, threadId: String): Mono<BannedUserViewEntity>
+	fun findByThreadId(threadId: String, pageable: Pageable): Flux<BannedUserViewEntity>
+	fun countByThreadId(threadId: String): Mono<Long>
+}
