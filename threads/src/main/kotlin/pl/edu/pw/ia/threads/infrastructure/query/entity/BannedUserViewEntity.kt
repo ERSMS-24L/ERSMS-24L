b332485241a35310ab2fdc3f95a7fbaef9bc38b9
Konrad Wojda <konrad2001@outlook.com>
2024-05-25 15:44:46 +0200

    Add banned user view entity

---
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/infrastructure/query/entity/BannedUserViewEntity.kt
@@ -0,0 +1,30 @@
+package pl.edu.pw.ia.threads.infrastructure.query.entity
+
+import java.util.UUID
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import pl.edu.pw.ia.shared.domain.view.BannedUserView
+
+@Document(collection = "bannedUserViews")
+data class BannedUserViewEntity(
+	@Id
+	val bannedUserId: String,
+	val threadId: String,
+	val accountId: String,
+) {
+	fun toDomain(): BannedUserView =
+		BannedUserView(
+			bannedUserId = UUID.fromString(bannedUserId),
+			threadId = UUID.fromString(threadId),
+			accountId = UUID.fromString(accountId)
+		)
+
+	companion object {
+		fun BannedUserView.toEntity(): BannedUserViewEntity =
+			BannedUserViewEntity(
+				bannedUserId = bannedUserId.toString(),
+				threadId = threadId.toString(),
+				accountId = accountId.toString(),
+			)
+	}
+}
