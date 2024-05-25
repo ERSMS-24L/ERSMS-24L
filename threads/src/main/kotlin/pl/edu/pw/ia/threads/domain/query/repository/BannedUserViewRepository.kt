1ead3b969f2bf6f99a9d032198dbed4897c11269
Konrad Wojda <konrad2001@outlook.com>
2024-05-25 15:44:56 +0200

    Add banned user view repository

---
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/domain/query/repository/BannedUserViewRepository.kt
@@ -0,0 +1,17 @@
+package pl.edu.pw.ia.threads.domain.query.repository
+
+import java.util.UUID
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import pl.edu.pw.ia.shared.domain.view.BannedUserView
+
+interface BannedUserViewRepository {
+
+	fun save(bannedUser: BannedUserView)
+
+	fun findByAccountIdAndThreadId(accountId: UUID, threadId: UUID): BannedUserView?
+
+	fun findByThreadId(threadId: UUID, pageable: Pageable): Page<BannedUserView>
+
+	fun delete(bannedUserId: UUID)
+}
