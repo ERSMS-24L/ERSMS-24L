d8058ba33f64ff8020993eb7eb2c971fbcdfc858
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2024-05-25 22:20:07 +0200

    [feat][k8s] Update deployment script

--- deploy-to-kubernetes.sh
+++ deploy-to-kubernetes.sh
@@ -22,9 +22,13 @@ kubectl apply -f https://download.elastic.co/downloads/eck/2.12.1/operator.yaml
 #helm install eck-stack elastic/eck-stack \
 #    --values https://raw.githubusercontent.com/elastic/cloud-on-k8s/2.12/deploy/eck-stack/examples/logstash/basic-eck.yaml
 kubectl apply -f k8s/init-logging/14-filebeat-config.yaml
-helm install eck-stack elastic/eck-stack --values k8s/init-logging/15-logging-config.yaml
+helm upgrade --install eck-stack elastic/eck-stack --values k8s/init-logging/15-logging-config.yaml
 kubectl get secret elasticsearch-es-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode; echo
+# user: elastic
 kubectl port-forward svc/eck-stack-eck-kibana-kb-http 5601:5601
+# address: https://localhost:5601/login
+# Hamburger -> Management -> Kibana -> Data Views ->
+# https://localhost:5601/app/management/kibana/dataViews
 
 # Monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
@@ -34,8 +38,8 @@ helm upgrade --install prometheus-stack prometheus-community/kube-prometheus-sta
 kubectl port-forward svc/prometheus-stack-kube-prom-prometheus 9090:9090
 kubectl port-forward svc/prometheus-stack-grafana 8080:80
 # spring dashboard: 19004
- kubectl scale deployment prometheus-stack-grafana --replicas=1
- kubectl scale deployment prometheus-stack-grafana --replicas=1
+kubectl scale deployment prometheus-stack-grafana --replicas=1
+kubectl scale deployment prometheus-stack-grafana --replicas=1
 
 # Verification
 kubectl get svc
@@ -44,6 +48,7 @@ kubectl get all
 
 # Delete Logging
 helm uninstall eck-stack
+helm uninstall prometheus-stack
 
 # Delete everything else
 kubectl delete all --all --all-namespaces
