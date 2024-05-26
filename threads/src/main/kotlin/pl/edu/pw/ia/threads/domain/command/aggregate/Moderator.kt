09087383a668a4ca460827c453da470ef92d2390
Konrad Wojda <konrad2001@outlook.com>
2024-05-26 15:27:26 +0200

    check account ids when adding/removing moderators

--- threads/src/main/kotlin/pl/edu/pw/ia/threads/domain/command/aggregate/Moderator.kt
+++ threads/src/main/kotlin/pl/edu/pw/ia/threads/domain/command/aggregate/Moderator.kt
@@ -4,11 +4,16 @@ import java.util.UUID
 import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.modelling.command.AggregateIdentifier
 import org.axonframework.modelling.command.AggregateLifecycle
+import org.axonframework.queryhandling.QueryGateway
 import org.axonframework.spring.stereotype.Aggregate
+import org.springframework.beans.factory.annotation.Autowired
 import pl.edu.pw.ia.shared.domain.command.AddModeratorCommand
 import pl.edu.pw.ia.shared.domain.command.RemoveModeratorCommand
 import pl.edu.pw.ia.shared.domain.event.ModeratorAddedEvent
 import pl.edu.pw.ia.shared.domain.event.ModeratorRemovedEvent
+import pl.edu.pw.ia.shared.domain.exception.AccountIsNotOwnerException
+import pl.edu.pw.ia.shared.domain.query.FindThreadByIdQuery
+import pl.edu.pw.ia.shared.domain.view.ThreadView
 
 @Aggregate
 internal class Moderator {
@@ -18,11 +23,17 @@ internal class Moderator {
 	private lateinit var accountId: UUID
 	private lateinit var threadId: UUID
 
+	@Autowired
+	private lateinit var queryGateway: QueryGateway
+
 	private constructor()
 
 	@CommandHandler
 	constructor(command: AddModeratorCommand) {
-		// TODO: Check if command.accountId is author of command.threadId
+		val threadView = queryGateway.query(FindThreadByIdQuery(command.threadId), ThreadView::class.java).join()
+		if(command.accountId != threadView.accountId){
+			throw AccountIsNotOwnerException(command.accountId, threadView.accountId)
+		}
 		AggregateLifecycle.apply(
 			ModeratorAddedEvent(
 				moderatorId = command.moderatorId,
@@ -34,7 +45,10 @@ internal class Moderator {
 
 	@CommandHandler
 	fun handle(command: RemoveModeratorCommand) {
-		// TODO: Check if command.accountId is author of command.threadId
+		val threadView = queryGateway.query(FindThreadByIdQuery(threadId), ThreadView::class.java).join()
+		if(command.accountId != threadView.accountId){
+			throw AccountIsNotOwnerException(command.accountId, threadView.accountId)
+		}
 		AggregateLifecycle.apply(
 			ModeratorRemovedEvent(
 				moderatorId = command.moderatorId,
